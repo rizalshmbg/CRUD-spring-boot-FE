5a41bf05fc68fdec74b8eb03c139d62aa0b35c3e
rizalshmbg <sihombingrizal18@gmail.com>
2024-07-02 19:23:20 +0700

    complete the deleteCar func

--- src/api/car-api.ts
+++ src/api/car-api.ts
@@ -1,8 +1,14 @@
 import { CarResponse } from '../types';
 import axios from 'axios';
 
+// GET ALL CARS FUNC
 export const getCars = async (): Promise<CarResponse[]> => {
 	const resp = await axios(`${import.meta.env.VITE_API_URL}/api/cars`);
 
 	return resp.data._embedded.cars;
+};
+
+export const deleteCar = async (link: string): Promise<CarResponse> => {
+	const resp = await axios.delete(link);
+	return resp.data;
 };
--- src/components/CarList.tsx
+++ src/components/CarList.tsx
@@ -1,24 +1,63 @@
-import { useQuery } from '@tanstack/react-query';
-import { getCars } from '../api/car-api';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import { getCars, deleteCar } from '../api/car-api';
+import { DataGrid, GridColDef, GridCellParams } from '@mui/x-data-grid';
+import { Snackbar } from '@mui/material';
+import { useState } from 'react';
 
 const CarList = () => {
+	const [openSnackbar, setOpenSnackbar] = useState(false);
+
+	const queryClient = useQueryClient();
+
 	const { data, error, isSuccess } = useQuery({
 		queryKey: ['cars'],
 		queryFn: getCars,
 	});
 
+	const { mutate } = useMutation(deleteCar, {
+		onSuccess: () => {
+			setOpenSnackbar(true);
+			queryClient.invalidateQueries({ queryKey: ['cars'] });
+		},
+		onError: (err) => {
+			console.error(err);
+		},
+	});
+
 	const columns: GridColDef[] = [
-		{ field: 'brand', headerName: 'Brand', width: 200 },
-		{ field: 'model', headerName: 'Model', width: 200 },
-		{ field: 'color', headerName: 'Color', width: 200 },
+		{ field: 'brand', headerName: 'Brand', width: 170 },
+		{ field: 'model', headerName: 'Model', width: 170 },
+		{ field: 'color', headerName: 'Color', width: 170 },
 		{
 			field: 'registrationNumber',
 			headerName: 'Registration Number',
-			width: 200,
+			width: 170,
 		},
 		{ field: 'modelYear', headerName: 'Model Year', width: 150 },
 		{ field: 'price', headerName: 'Price', width: 150 },
+		{
+			field: 'delete',
+			headerName: '',
+			width: 90,
+			sortable: false,
+			filterable: false,
+			disableColumnMenu: true,
+			renderCell: (params: GridCellParams) => (
+				<button
+					onClick={() => {
+						if (
+							window.confirm(
+								`Are you sure you want delete the car ${params.row.brand} ${params.row.model}`
+							)
+						) {
+							mutate(params.row._links.car.href);
+						}
+					}}
+				>
+					Delete
+				</button>
+			),
+		},
 	];
 
 	if (!isSuccess) {
@@ -27,11 +66,20 @@ const CarList = () => {
 		return <span>Error when fetching cars...</span>;
 	} else {
 		return (
-			<DataGrid
-				rows={data}
-				columns={columns}
-				getRowId={(row) => row._links.self.href}
-			/>
+			<>
+				<DataGrid
+					rows={data}
+					columns={columns}
+					getRowId={(row) => row._links.self.href}
+					disableRowSelectionOnClick={true}
+				/>
+				<Snackbar
+					open={openSnackbar}
+					autoHideDuration={2000}
+					onClose={() => setOpenSnackbar(false)}
+					message='Car deleted'
+				/>
+			</>
 		);
 	}
 };
