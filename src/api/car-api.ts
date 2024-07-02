2bc132491a7e1d48007823391206acf131b07ddc
rizalshmbg <sihombingrizal18@gmail.com>
2024-07-02 21:20:45 +0700

    complete the addCar func

--- src/api/car-api.ts
+++ src/api/car-api.ts
@@ -1,5 +1,5 @@
-import { CarResponse } from '../types';
 import axios from 'axios';
+import { Car, CarResponse } from '../types';
 
 // GET ALL CARS FUNC
 export const getCars = async (): Promise<CarResponse[]> => {
@@ -8,7 +8,23 @@ export const getCars = async (): Promise<CarResponse[]> => {
 	return resp.data._embedded.cars;
 };
 
+// DELETE CAR FUNC
 export const deleteCar = async (link: string): Promise<CarResponse> => {
 	const resp = await axios.delete(link);
+	return resp.data;
+};
+
+// ADD NEW CAR FUNC
+export const addCar = async (car: Car): Promise<CarResponse> => {
+	const resp = await axios.post(
+		`${import.meta.env.VITE_API_URL}/api/cars`,
+		car,
+		{
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		}
+	);
+
 	return resp.data;
 };
