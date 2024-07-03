879f2c0a0a1fd4880850e5f49e57c76c31783892
rizalshmbg <sihombingrizal18@gmail.com>
2024-07-03 21:49:31 +0700

    refactor code by using AxiosRequestConfig method

--- src/api/car-api.ts
+++ src/api/car-api.ts
@@ -1,42 +1,42 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { Car, CarResponse } from '../types';
 import { CarEntry } from './../types';
 
-// GET ALL CARS FUNC
-export const getCars = async (): Promise<CarResponse[]> => {
+// CONFIG FOR AXIOS
+const getAxiosConfig = (): AxiosRequestConfig => {
 	const token = sessionStorage.getItem('jwt');
 
-	const resp = await axios(`${import.meta.env.VITE_API_URL}/api/cars`, {
-		headers: { Authorization: token },
-	});
+	return {
+		headers: {
+			Authorization: token,
+			'Content-Type': 'application/json',
+		},
+	};
+};
+
+// GET ALL CARS FUNC
+export const getCars = async (): Promise<CarResponse[]> => {
+	const resp = await axios(
+		`${import.meta.env.VITE_API_URL}/api/cars`,
+		getAxiosConfig()
+	);
 
 	return resp.data._embedded.cars;
 };
 
 // DELETE CAR FUNC
 export const deleteCar = async (link: string): Promise<CarResponse> => {
-	const token = sessionStorage.getItem('jwt');
-
-	const resp = await axios.delete(link, {
-		headers: { Authorization: token },
-	});
+	const resp = await axios.delete(link, getAxiosConfig());
 
 	return resp.data;
 };
 
 // ADD NEW CAR FUNC
 export const addCar = async (car: Car): Promise<CarResponse> => {
-	const token = sessionStorage.getItem('jwt');
-
 	const resp = await axios.post(
 		`${import.meta.env.VITE_API_URL}/api/cars`,
 		car,
-		{
-			headers: {
-				'Content-Type': 'application/json',
-				Authorization: token,
-			},
-		}
+		getAxiosConfig()
 	);
 
 	return resp.data;
@@ -44,14 +44,7 @@ export const addCar = async (car: Car): Promise<CarResponse> => {
 
 // EDIT CAR FUNC
 export const editCar = async (carEntry: CarEntry): Promise<CarResponse> => {
-	const token = sessionStorage.getItem('jwt');
-
-	const resp = await axios.patch(carEntry.url, carEntry.car, {
-		headers: {
-			'Content-Type': 'application/json',
-			Authorization: token,
-		},
-	});
+	const resp = await axios.patch(carEntry.url, carEntry.car, getAxiosConfig());
 
 	return resp.data;
 };
